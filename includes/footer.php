8e891814b0b347610d029e7ee26bb17e28463ae5
Rplantey <remi.plantey98@gmail.com>
2018-11-30 16:11:32 +0100

    fin footer

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<footer class="container-fluid bg_gray">
+	<hr>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 center_footer">
+				<a href="https://www.facebook.com/">
+					<i class="fab fa-facebook footer_ico"></i>
+				</a>
+				<a href="https://www.twitter.com/">
+					<i class="fab fa-twitter-square footer_ico"></i>
+				</a>
+				<a href="https://www.instagram.com/">
+					<i class="fab fa-instagram footer_ico"></i>
+				</a>
+				<a href="https://www.pinterest.com/">
+					<i class="fab fa-pinterest-square footer_ico"></i>
+				</a>
+
+			</div>
+			<div class="col-12 col-md-6">
+				<div class="row">
+					<div class="col-12 col-md-6 footer_contact">
+						<ul>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="mention_legal.php">Mentions legales</a></li>
+							<li><a href="contact.php">Newsletter</a></li>
+						</ul>
+					</div>
+					<div class="col-12 col-md-6 les_meilleurs">
+						<ul>
+							<li>Mika EL proffessor</li>
+							<li>Thomas</li>
+							<li>Christophe</li>
+							<li>Clément</li>
+							<li>Rémi</li>
+						</ul>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+</footer>
