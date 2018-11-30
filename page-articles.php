59615838bcbcd7d4ea04e16a71998dbd911de263
totoff24 <christophecarrerot@wanadoo.fr>
2018-11-30 15:42:38 +0100

    changement articles
    changement page articles

--- page-articles.php
+++ page-articles.php
@@ -1,23 +1,83 @@
+<?php
+include('connect.php');
+session_start();
+
+$select = $connexion->query('SELECT * FROM categories');
+$categories = $select->fetchAll();
+
+?>
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title>Articles</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	<link rel="stylesheet" href="page-articles.css">
 </head>
 <body>
 
 <main class="container">
 
-	<dir class="row">
+	<form method="GET" id="form-search"class="d-flex align-items-center">	
 		
-		<div class="col-md-3"></div>
-		<div class="col-md-3"></div>
-		<div class="col-md-2"></div>
-		<div class="col-md-offset-2"></div>
-		<div class="col-md-2"></div>
 
-	</dir>
+		<div class="col-md-4 form-group d-flex justify-content-center align-items-center">
+			<input type="text" name="nom" placeholder="Nom" id="nom">
+			<button type="submit" class="btn btn-info" id="search"><i class="fas fa-search"></i></button>
+		</div>
+		<div class="col-md-3 form-group">
+			<select name="categories">
+				<option value="0">Catégories</option>
+				<?php 
+					foreach ($categories as $categorie) {
+						?>
+						<option value="$categorie['id']"><?= $categorie['category_name']?></option>
+						<?php
+					}
+						?>
+
+			</select>
+		</div>
+		
+		
+			
+		
+		<div class="col-md-2 offset-md-2 form-group text-right">
+			<select name="tri">
+				<option value="0">Ordre</option>
+				<option value="asc">Croissant</option>
+				<option value="desc">Décroissant</option>
+			</select>
+		</div>
+		
+    </form>
+
+
+    <!--ligne article--> 	
+    <div class="row align-items-center" id="rowArticle">
+    	<div class="col-md-4">
+    		<img src="https://placehold.it/300x200" alt="">
+    	</div>
+    	<div class="col-md-3">
+    		<h1>Nom produit</h1>
+    		<h2>Catégorie</h2>
+    	</div>
+    	<div class="col-md-3">
+    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet facilisis velit. Maecenas massa quam, laoreet id tincidunt quis, blandit ut nunc. Sed faucibus convallis odio, non hendrerit ex rhoncus non.</p>
+    	</div>
+    	<div class="col-md-2 text-right">
+    		<h3>50$</h3>
+    	</div>
+    </div>
 	
 </main>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
