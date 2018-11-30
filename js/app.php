ef182da8f21727a9200da8839968c5fe4fbd5b55
mnaxara <45257561+mnaxara@users.noreply.github.com>
2018-11-30 14:53:24 +0100

    slider php ok

--- js/app.php
+++ js/app.php
@@ -1,7 +1,6 @@
 <script type="text/javascript">let $background = [];</script>
-<?php
 
-require_once ('includes/connect.php');
+<?php
 
 $backgroundSearch = $connexion->query("
 	
@@ -13,21 +12,18 @@ $backgrounds = $backgroundSearch->fetchAll();
 
 foreach ($backgrounds as $background) {
 	?><script type="text/javascript">
-		$background.push('url(' + <?=$background['name']?>+')');
+		$background.push('url(../img/<?=$background['name']?>)');
 
 	</script>
+	<?php
 }
 
 ?>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
 <script type="text/javascript">
 $(function(){ // Encapsulage
  
 	// CHANGEMENT DU BG DU HEADER
-
-	<?php?>
-
-	
 
 	let backActuel = 0; // Declaration d'une variable qui stockera le fond d'ecran actuel (index)
 
