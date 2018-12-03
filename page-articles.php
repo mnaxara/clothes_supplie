a438cc707ce902237d302f1e5ea9415816da62d3
totoff24 <christophecarrerot@wanadoo.fr>
2018-12-03 17:03:19 +0100

    maj page articles

--- page-articles.php
+++ page-articles.php
@@ -13,6 +13,7 @@ $categories = $select->fetchAll();
 	<meta charset="utf-8">
 	<?php include ('includes/link.php')?>
 	<title>Articles</title>
+	
 </head>
 
 <body>
@@ -24,57 +25,106 @@ $categories = $select->fetchAll();
 	<form method="GET" id="form-search"class="d-flex align-items-center">	
 
 
-		<div class="col-md-4 form-group d-flex justify-content-center align-items-center">
+		<div class="col-md-6 form-group d-flex align-items-center">
 			<input type="text" name="nom" placeholder="Nom" id="nom">
-			<button type="submit" class="btn btn-info" id="search"><i class="fas fa-search"></i></button>
-		</div>
-		<div class="col-md-3 form-group">
-			<select name="categories">
+			
+		
+			<select name="categorie">
 				<option value="0">Catégories</option>
 				<?php 
 					foreach ($categories as $categorie) {
 						?>
-						<option value="$categorie['id']"><?= $categorie['category_name']?></option>
+						<option value="<?php echo $categorie['id']?>">
+						<?=$categorie['category_name']?>
+						</option>
 						<?php
 					}
 						?>
 
 			</select>
+			<button type="submit" class="btn btn-info" id="search"><i class="fas fa-search"></i></button>
 		</div>
 		
-		<div class="col-md-2 offset-md-2 form-group text-right">
+		<div class="col-md-2 offset-md-4 form-group text-right">
 			<select name="tri">
 				<option value="0">Ordre</option>
-				<option value="asc">Croissant</option>
-				<option value="desc">Décroissant</option>
+				<option value="ASC">Croissant</option>
+				<option value="DSC">Décroissant</option>
 			</select>
 		</div>
 		
     </form>
 	
-    <div class="row align-items-center" id="rowArticle">
-    	<div class="col-md-4">
-    		<img src="https://placehold.it/300x200" alt="">
-    	</div>
-    	<div class="col-md-3">
-    		<h1>Nom produit</h1>
-    		<h2>Catégorie</h2>
-    	</div>
-    	<div class="col-md-3">
-    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet facilisis velit. Maecenas massa quam, laoreet id tincidunt quis, blandit ut nunc. Sed faucibus convallis odio, non hendrerit ex rhoncus non.</p>
-    	</div>
-    	<div class="col-md-2 text-right">
-    		<h3>50$</h3>
-    	</div>
-    </div>
+    
 
-	<?php 
 
-	if(!empty($_GET)){
-		$select = $connexion -> query('SELECT * FROM categories');
-		$recherche = $select ->fetchAll();
+
+
+
+
+
+    
+<?php 
+    $select = $connexion->query('SELECT COUNT(id) AS nb  FROM products');
+	$count = $select->fetch();
+	$nb_products = $count['nb'];
+
+	$nb_par_page = 5;
+
+	$nbPages = ceil($nb_products / $nb_par_page);
+
+	if(empty($_GET['numeroPage']) || !is_numeric($_GET['numeroPage']) || $_GET['numeroPage'] > $nbPages){
+     //par défaut on affiche la première page
+	$numeroPage = 1;
+    }
+    else{
+	$numeroPage =  $_GET['numeroPage'];
+    }
+
+    $offset = ($numeroPage - 1) * $nb_par_page;
+
+
+
+	
+
+	if(empty($_GET)){
+
+	$select = $connexion -> query("SELECT *, FROM products INNER JOIN categories_has_products ON products.id = categories_has_products.products_id INNER JOIN categories ON categories_has_products.categories_id = categories.id LIMIT " . $offset . ",". $nb_par_page);
+		$products = $select -> fetchAll();
+
+		$selection = $connexion->query("SELECT * FROM products INNER JOIN imgproduct ON products.id = imgproduct.id_product");
+		$images = $selection->fetchAll();
+
+		foreach ($products as $product) {
+		?>
+			<div class="row align-items-center" id="rowArticle">
+
+    			<div class="col-md-4">
+    				<img src="img/thumbnails/<?= $images[0]['name']?>" alt="Ma bite">
+    			</div>
+
+    			<div class="col-md-3">
+    				<h2><?=$product['name']?></h2>
+    				<h3>Catégorie</h3>
+    			</div>
+
+    			<div class="col-md-3">
+    				<p><?= $product['description']?></p>
+    			</div>
+
+    			<div class="col-md-2 text-right">
+    				<h3><?=$product['price']?>€</h3>
+    			</div>
+    		</div>
+    	<?php
+		}
+		
+
+	}
+	else{
+
 		$errors = [];
-		if(!empty($_GET['categorie']) && !in_array($_GET['categorie'], $recherche)){
+		if(!empty($_GET['categorie']) && !is_numeric($_GET['categorie'])){
 			$errors[] = 'Categorie invalide';
 		}
 		if(!empty($_GET['nom']) && !preg_match('#\w{2,20}#', $_GET['nom'])){
@@ -84,16 +134,18 @@ $categories = $select->fetchAll();
 			$errors[] = 'tri invalide';
 		}
 		if(empty($errors)){
-			$sql = 'SELECT * FROM products INNER JOIN categories_has_products ON products.id = categorie_has_products.id_products INNER JOIN categories ON categorie_has_products.categories_id = categories.id';
+			$sql = 'SELECT * FROM products INNER JOIN categories_has_products ON products.id = categories_has_products.products_id INNER JOIN categories ON categories_has_products.categories_id = categories.id';
 			if(!empty($_GET['categorie'])){
-				$sql .= ' WHERE :categorie';
+				$sql .= ' WHERE categories.id = :categorie';
 			}
 			if(!empty($_GET['nom'])){
-				$sql .= ' AND nom LIKE :nom ';
+				$sql .= ' AND name LIKE :nom ';
 			}
 			if(!empty($_GET['tri'])){
 				$sql .= ' ORDER BY price :ordre';
 			}
+			$sql .= ' LIMIT ' . $offset . ',' . $nb_par_page;
+			echo $sql;
 			$select = $connexion -> prepare($sql);
 			if(!empty($_GET['categorie'])){
 				$select -> bindValue(':categorie', strip_tags($_GET['categorie']));
@@ -101,18 +153,64 @@ $categories = $select->fetchAll();
 			if(!empty($_GET['nom'])){
 				$select -> bindValue(':nom', strip_tags($_GET['nom']));
 			}
-			if(isset($_GET['tri'])){
+			if(!empty($_GET['tri']) && $_GET['tri'] === 'ASC'){
+				$select -> bindValue(':ordre', $_GET['tri']);
+			}
+			if(!empty($_GET['tri']) && $_GET['tri'] === 'DESC'){
 				$select -> bindValue(':ordre', $_GET['tri']);
 			}
 			$select->execute();
         	$products = $select->fetchAll();
 
-        	var_dump($products);
+        	$selection = $connexion->query("SELECT * FROM products INNER JOIN imgproduct ON products.id = imgproduct.id_product");
+			$images = $selection->fetchAll();
+			foreach ($products as $product) {
+			?>
+			<div class="row align-items-center" id="rowArticle">
+    			<div class="col-md-4">
+    		<img src="img/thumbnails/<?= $images[0]['name'] ?>" alt="Ma bite">
+    	</div>
+    	<div class="col-md-3">
+    		<h2><?=$product['name']?></h2>
+    		<h3><?=$product['category_name']?></h3>
+    	</div>
+    	<div class="col-md-3">
+    		<p><?= $product['description']?> <br>
+    			Il reste <?=$product['availability']?>
+    		</p>
+    	</div>
+    	<div class="col-md-2 text-right">
+    		<h3><?=$product['price']?>€</h3>
+    	</div>
+    </div>
+    	<?php
+		}
+
 		}
 		else{
 			echo implode('<br>', $errors);
 		}
 	}
+
+    if ($numeroPage > 1) {
+   	
+   
+	?>
+	<a href="page-articles.php?numeroPage=<?=$numeroPage -1?>">Page précédente</a>
+<?php
+  }
+      for ($i=1; $i <= $nbPages; $i++) { 
+      	?>
+      	<a href="page-articles.php?numeroPage=<?= $i ?>">Page <?= $i ?></a>
+      	<?php
+      }
+      if ($numeroPage < $nbPages) {
+      
+?>
+	<a href="page-articles.php?numeroPage=<?=$numeroPage +1 ?>">Page suivante </a>
+<?php
+}
+?>
 	
 	?>
 </main>
